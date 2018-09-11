f8138b49eb69a2cb2e4f176ed7f6abd77508c213
ahmedbenouezdou <ahmed.b.ouezdou@gmail.com>
2018-09-11 23:38:10 +0200

    update

--- src/app/pages/_services/authentication.service.ts
+++ src/app/pages/_services/authentication.service.ts
@@ -11,11 +11,7 @@ export class AuthenticationService {
     constructor(private http: HttpClient) { }
 
     login(username: string, password: string) {
-        var headers_object = new HttpHeaders();
         const options = new HttpParams();
-        console.log(Md5.init(password));
-        headers_object.append('Content-Type', 'application/json');
-
         return this.http.post<any>(`http://localhost:8082/getToken`, {'username':username,'password':Md5.init(password)})
             .pipe(map(dataToken => {
                 // login successful if there's a jwt token in the response
