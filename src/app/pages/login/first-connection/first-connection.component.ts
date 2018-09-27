648d6ed760b1b1ad6b7428bf1c2979415365ab01
ahmedbenouezdou <ahmed.b.ouezdou@gmail.com>
2018-09-27 08:06:15 +0200

    update

--- src/app/pages/login/first-connection/first-connection.component.ts
+++ src/app/pages/login/first-connection/first-connection.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { first } from 'rxjs/operators';
+
+import { ConfigLogin } from '../login';
 
 @Component({
   selector: 'app-first-connection',
@@ -7,9 +11,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FirstConnectionComponent implements OnInit {
 
-  constructor() { }
+  loginModel = new ConfigLogin();
+  loading = false;
+  submitted = false;
+  returnUrl: string;
+  error = '';
+
+  constructor(
+      private route: ActivatedRoute,
+      private router: Router) {}
 
   ngOnInit() {
+
+      // reset login status
+      // get return url from route parameters or default to '/'
+      this.returnUrl =  '/dashboard/home';
   }
+
+
+
+  onSubmit() {
+   this.submitted = true;
+        this.loading = true;
+        /*this.authenticationService.login(this.loginModel.email, this.loginModel.password)
+            .pipe(first())
+            .subscribe(
+                data => {
+                    this.router.navigate([this.returnUrl]);
+                },
+                error => {
+                    this.error = error;
+                    this.loading = false;
+                });*/
+
+
+    }
+
 
 }
